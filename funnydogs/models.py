47f9c1e4bfb2ef5da83b6275ed95efaf8abd5c31
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-11-01 20:51:49 +0100

    New Class Created

--- funnydogs/models.py
+++ funnydogs/models.py
@@ -8,7 +8,17 @@ class Author(models.Model):
 
 class Entry(models.Model):
     title = models.CharField(max_length=200)
-    content = models.CharField(max_length=1000)
+    content = models.CharField(max_length=3000)
     time_stamp = models.DateTimeField('date published')
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+	content = models.CharField(max_length=1000)
+	time_stamp = models.DateTimeField(blank=True, default=datetime.datetime.now)
+	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+
+# extend models
+# add comment
 
