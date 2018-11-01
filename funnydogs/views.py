afdfdfa7278b1cefa7616ed5d90f30eff30b4c38
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-11-01 21:36:39 +0100

    Edited Webpage Entries for Practice

--- funnydogs/views.py
+++ funnydogs/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from funnydogs.models import Entry
+
 
 def index(request):
-	return HttpResponse('Nessie is cute.')
+	entries = Entry.objects.all()
+	result = ''
+
+	for entry in entries:
+		result += str(entry.content) + '<br>'
+
+	return HttpResponse(result)
 
 def second_page(request):
 	return HttpResponse('I guess this is the second page.')
