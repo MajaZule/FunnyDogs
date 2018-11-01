b4925873c7ef4056e898e0ae5284cea5ce749233
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-11-01 11:36:04 +0100

    Updated the URL resolve info

--- funnydogs/urls.py
+++ funnydogs/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name = 'index'),
-	path('2/', views.second_page, name = 'second_page')
+	path('7/', views.second_page, name = 'second_page')
 ]
