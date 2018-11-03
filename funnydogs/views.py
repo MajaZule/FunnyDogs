fcc4ddafa08eeb2303fec5784e929aec3cdabf66
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-11-03 19:08:20 +0100

    Another html

--- funnydogs/views.py
+++ funnydogs/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from funnydogs.models import Entry
+from django.template import loader
+from .models import Entry
 
 
 def index(request):
-	entries = Entry.objects.all().order_by('-time_stamp')[:10]
-	result = ''
+	latest_entries_list = Entry.objects.all().order_by('-time_stamp')[:10]
+	template = loader.get_template('funnydogs/index.html')
+	result = {
+		'latest_entries_list':latest_entries_list,
+	}
 
-	for entry in entries:
-		result += str(entry.content) + '<br>'
-
-	return HttpResponse(result)
+	return HttpResponse(template.render(result, request))
 
 def entry(request, entry_id):
-	pass
+	try:
+		entry = Entry.objects.get(pk=entry_id)
+	except Entry.DoesNotExist:
+		raise Http404('This entry does not exist.')
+	return render(request, 'funnydogs/entry.html', {'entry': entry})
 
 def create_entry(request):
 	return HttpResponse('Page where user creates new entries.')
