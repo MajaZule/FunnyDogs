a997be5a6b5f6fbb663f815e2d65bd722920b612
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-11-04 14:51:24 +0100

    Views Simplified

--- funnydogs/urls.py
+++ funnydogs/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name = 'index'),
-	path('<int:entry_id>/', views.entry, name = 'entry'),
+	path('', views.IndexView.as_view(), name = 'index'),
+	path('<int:pk>/', views.EntryView.as_view(), name = 'entry'),
 	path('create/', views.create_entry, name = 'create_entry'),
 	path('personal/', views.personal_page, name = 'personal_page'),
 ]
--- funnydogs/views.py
+++ funnydogs/views.py
@@ -1,25 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from funnydogs.models import Entry
 from django.template import loader
 from .models import Entry
+from django.views import generic
+from django.urls import reverse
+
+from .models import Entry
 
 
-def index(request):
-	latest_entries_list = Entry.objects.all().order_by('-time_stamp')[:10]
-	template = loader.get_template('funnydogs/index.html')
-	result = {
-		'latest_entries_list':latest_entries_list,
-	}
+class IndexView(generic.ListView):
+	template_name = 'funnydogs/index.html'
+	context_object_name = 'latest_entries_list'
+	
+	def get_queryset(self):
+		return Entry.objects.order_by('-time_stamp')[:10]
 
-	return HttpResponse(template.render(result, request))
-
-def entry(request, entry_id):
-	try:
-		entry = Entry.objects.get(pk=entry_id)
-	except Entry.DoesNotExist:
-		raise Http404('This entry does not exist.')
-	return render(request, 'funnydogs/entry.html', {'entry': entry})
+class EntryView(generic.DetailView):
+	model = Entry
+	template_name = 'funnydogs/entry.html'
 
 def create_entry(request):
 	return HttpResponse('Page where user creates new entries.')
